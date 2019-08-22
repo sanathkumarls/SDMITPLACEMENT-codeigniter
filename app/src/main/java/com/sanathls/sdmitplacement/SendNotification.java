4f93555070a97200c4d69fe6c335566edcff9b42
Sanath L S <sanathlslokanathapura@gmail.com>
2019-08-23 02:15:00 +0530

    send notification

--- app/src/main/java/com/sanathls/sdmitplacement/SendNotification.java
+++ app/src/main/java/com/sanathls/sdmitplacement/SendNotification.java
@@ -4,13 +4,22 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class SendNotification extends AppCompatActivity {
+
+    EditText title_send,message,link;
+    String title,descrition,web_link;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.send_notification_layout);
+
+        title_send=findViewById(R.id.title_send);
+        message=findViewById(R.id.message);
+        link=findViewById(R.id.link);
     }
 
     public void go_back_to_dashboard(View view)
@@ -18,6 +27,12 @@ public class SendNotification extends AppCompatActivity {
         finish();
     }
 
-    public void send_notification(View view) {
+    public void send_notification(View view)
+    {
+        title=title_send.getText().toString();
+        descrition=message.getText().toString();
+        web_link=link.getText().toString();
+
+        Toast.makeText(this,title+"\n"+descrition+"\n"+web_link,Toast.LENGTH_LONG).show();
     }
 }
