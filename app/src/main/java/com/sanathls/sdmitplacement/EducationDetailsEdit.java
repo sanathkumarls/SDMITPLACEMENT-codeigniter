e85bb7aef1cab709f34cc126f0976f8f3e40bad3
Sanath L S <sanathlslokanathapura@gmail.com>
2019-09-03 20:27:45 +0530

    education details edit

--- app/src/main/java/com/sanathls/sdmitplacement/EducationDetailsEdit.java
+++ app/src/main/java/com/sanathls/sdmitplacement/EducationDetailsEdit.java
@@ -3,12 +3,15 @@ package com.sanathls.sdmitplacement;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
 public class EducationDetailsEdit extends AppCompatActivity {
 
     String user_email,user_name,user_usn,sslc,puc,sem1,sem2,sem3,sem4,sem5,sem6,sem7,cgpa;
+    EditText Etsslc;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,9 +33,30 @@ public class EducationDetailsEdit extends AppCompatActivity {
         sem7=bundle.getString("sem7");
         cgpa=bundle.getString("cgpa");
 
-        String received=user_email+"\n"+user_name+"\n"+user_usn+"\n"+sslc+"\n"+puc+"\n"+sem1+"\n"+sem2+"\n"+sem3+"\n"+sem4+"\n"+sem5+"\n"+sem6+"\n"+sem7+"\n"+cgpa;
+        //String received=user_email+"\n"+user_name+"\n"+user_usn+"\n"+sslc+"\n"+puc+"\n"+sem1+"\n"+sem2+"\n"+sem3+"\n"+sem4+"\n"+sem5+"\n"+sem6+"\n"+sem7+"\n"+cgpa;
 
-        Toast.makeText(this,received,Toast.LENGTH_LONG).show();
+        //Toast.makeText(this,received,Toast.LENGTH_LONG).show();
+
+        Etsslc=findViewById(R.id.Etsslc);
+        Etsslc.setText(sslc);
+
+
+    }
+
+    public void go_back_to_education_details(View view)
+    {
+        finish();
+    }
+
+    @Override
+    public void onBackPressed()
+    {
+        finish();
+    }
+
+    public void update_marks(View view)
+    {
+
 
     }
 }
