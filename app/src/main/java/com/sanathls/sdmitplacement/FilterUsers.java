b9980f6cc02042086c6dac02c0bca658e78aadb1
Sanath L S <sanathlslokanathapura@gmail.com>
2019-09-14 00:53:15 +0530

    filter layout complete

--- app/src/main/java/com/sanathls/sdmitplacement/FilterUsers.java
+++ app/src/main/java/com/sanathls/sdmitplacement/FilterUsers.java
@@ -3,12 +3,27 @@ package com.sanathls.sdmitplacement;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class FilterUsers extends AppCompatActivity {
+
+    String admin_email;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.layout_filter_users);
+        setContentView(R.layout.filter_users_layout);
+
+        Bundle bundle=getIntent().getExtras();
+        admin_email=bundle.getString("user_email");
+
+    }
+
+    public void go_back_to_dashboard(View view)
+    {
+        finish();
+    }
+
+    public void filter(View view) {
     }
 }
