25e37e17ebe24a582b4ecbac26273332afc08783
Sanath L S <sanathlslokanathapura@gmail.com>
2019-09-24 00:07:49 +0530

    project finished with export

--- app/src/main/java/com/sanathls/sdmitplacement/ViewUsers.java
+++ app/src/main/java/com/sanathls/sdmitplacement/ViewUsers.java
@@ -1,6 +1,7 @@
 package com.sanathls.sdmitplacement;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.FileProvider;
 
 import android.app.Activity;
 import android.app.AlertDialog;
@@ -8,6 +9,7 @@ import android.app.ProgressDialog;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Log;
@@ -23,6 +25,8 @@ import org.json.JSONObject;
 
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
@@ -83,6 +87,18 @@ public class ViewUsers extends AppCompatActivity {
     public void go_back_to_dashboard(View view)
     {
         finish();
+    }
+
+    public void export(View view)
+    {
+        progressDialog=new ProgressDialog(this);
+        progressDialog.setTitle("Exporting...");
+        progressDialog.setMessage("Please Wait");
+        progressDialog.setCancelable(false);
+        progressDialog.show();
+
+        ExportTask exportTask=new ExportTask(this,this,progressDialog);
+        exportTask.execute(admin_email,sslc,sslc_score,puc,puc_score,cgpa,cgpa_score);
     }
 }
 
@@ -299,6 +315,204 @@ class ViewUsersTask extends AsyncTask<String,String,String>
             else
             {
                 Toast.makeText(ctx,"No Users",Toast.LENGTH_LONG).show();
+            }
+
+        } catch (JSONException e) {
+
+            AlertDialog.Builder alertDialog = new AlertDialog.Builder(ctx);
+            //Setting Dialog Title
+            alertDialog.setTitle("Cannot Connect To Server !!!");
+            //Setting Dialog Icon
+            alertDialog.setIcon(R.mipmap.ic_launcher);
+            //Setting Dialog Message
+            alertDialog.setMessage("Check Your Internet Connection Or Try Again Later ...");
+
+            alertDialog.setCancelable(false);
+
+            //On Pressing Setting button
+            alertDialog.setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    dialog.dismiss();
+                    activity.finishAffinity();
+                }
+            });
+            alertDialog.show();
+        }
+
+    }
+}
+
+
+
+class ExportTask extends AsyncTask<String,String,String> {
+
+    Context ctx;
+    Activity activity;
+    ProgressDialog progressDialog;
+
+    ExportTask(Context ctx, Activity activity, ProgressDialog progressDialog) {
+        this.ctx = ctx;
+        this.activity = activity;
+        this.progressDialog = progressDialog;
+    }
+
+
+    @Override
+    protected void onPreExecute() {
+
+    }
+
+    @Override
+    protected String doInBackground(String... params) {
+
+        String admin_email = params[0];
+        String sslc = params[1];
+        String sslc_score = params[2];
+        String puc = params[3];
+        String puc_score = params[4];
+        String cgpa = params[5];
+        String cgpa_score = params[6];
+
+
+        try {
+            URL url = new URL(Constants.base_url + "adminapi/filter_users");
+            HttpURLConnection con = (HttpURLConnection) url.openConnection();
+            con.setRequestMethod("POST");
+            con.setDoOutput(true);
+            con.setDoInput(true);
+            OutputStream os = con.getOutputStream();
+            BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(os, "UTF-8"));
+            String data = URLEncoder.encode("admin_email", "UTF-8") + "=" + URLEncoder.encode(admin_email, "UTF-8") + "&" +
+                    URLEncoder.encode("sslc", "UTF-8") + "=" + URLEncoder.encode(sslc, "UTF-8") + "&" +
+                    URLEncoder.encode("sslc_score", "UTF-8") + "=" + URLEncoder.encode(sslc_score, "UTF-8") + "&" +
+                    URLEncoder.encode("puc", "UTF-8") + "=" + URLEncoder.encode(puc, "UTF-8") + "&" +
+                    URLEncoder.encode("puc_score", "UTF-8") + "=" + URLEncoder.encode(puc_score, "UTF-8") + "&" +
+                    URLEncoder.encode("cgpa", "UTF-8") + "=" + URLEncoder.encode(cgpa, "UTF-8") + "&" +
+                    URLEncoder.encode("cgpa_score", "UTF-8") + "=" + URLEncoder.encode(cgpa_score, "UTF-8");
+            bw.write(data);
+            bw.flush();
+            bw.close();
+            os.close();
+            InputStream is = con.getInputStream();
+            BufferedReader br = new BufferedReader(new InputStreamReader(is, "iso-8859-1"));
+            String response = "", line = "";
+            while ((line = br.readLine()) != null) {
+                response += line;
+            }
+            br.close();
+            is.close();
+            con.disconnect();
+            return response;
+
+        } catch (MalformedURLException e) {
+            Log.e("malformedurl", e.toString());
+            return "offline";
+        } catch (IOException e) {
+            Log.e("ioexcetion", e.toString());
+            return "offline";
+        }
+
+
+    }
+
+    @Override
+    protected void onPostExecute(String response) {
+
+        progressDialog.cancel();
+        //Toast.makeText(ctx,response,Toast.LENGTH_LONG).show();
+        Log.e("Response", response);
+
+        try {
+            JSONObject jsonObject = new JSONObject(response);
+            String result = jsonObject.getString("result");
+            if (result.equals("failure")) {
+                String message = jsonObject.getString("message");
+                Log.e("message", message);
+                Toast.makeText(ctx, message, Toast.LENGTH_LONG).show();
+            } else if (result.equals("success")) {
+                //Toast.makeText(this,"Login Success.",Toast.LENGTH_SHORT).show();
+                String arraysize = jsonObject.getString("size");
+                int size = Integer.parseInt(arraysize);
+
+                if (size == 0)
+                {
+                    Toast.makeText(ctx, "No Users Available", Toast.LENGTH_LONG).show();
+                }
+
+                String[] user_name = new String[size], user_email = new String[size], user_usn = new String[size], user_phone = new String[size];
+                String[] sslc = new String[size], puc = new String[size], sem1 = new String[size], sem2 = new String[size];
+                String[] sem3 = new String[size], sem4 = new String[size], sem5 = new String[size], sem6 = new String[size], sem7 = new String[size], cgpa = new String[size];
+
+                for (int i = 0; i < size; i++)
+                {
+                    user_name[i] = jsonObject.getString("user_name" + i);
+                    user_email[i] = jsonObject.getString("user_email" + i);
+                    user_usn[i] = jsonObject.getString("user_usn" + i);
+                    user_phone[i] = jsonObject.getString("user_phone" + i);
+                    sslc[i] = jsonObject.getString("sslc" + i);
+                    puc[i] = jsonObject.getString("puc" + i);
+                    sem1[i] = jsonObject.getString("sem1" + i);
+                    sem2[i] = jsonObject.getString("sem2" + i);
+                    sem3[i] = jsonObject.getString("sem3" + i);
+                    sem4[i] = jsonObject.getString("sem4" + i);
+                    sem5[i] = jsonObject.getString("sem5" + i);
+                    sem6[i] = jsonObject.getString("sem6" + i);
+                    sem7[i] = jsonObject.getString("sem7" + i);
+                    cgpa[i] = jsonObject.getString("cgpa" + i);
+                }
+
+                if(size > 0)
+                {
+                    StringBuilder data =new StringBuilder();
+                    data.append("Sl. No,Name,Email,USN,Phone,SSLC,PUC,SEM 1,SEM 2,SEM 3,SEM 4,SEM 5,SEM 6,SEM 7,CGPA");
+                    for(int i=0;i<user_name.length;i++)
+                    {
+                        data.append(
+                                "\n"+
+                                        (i+1)+","+
+                                        user_name[i]+","+
+                                        user_email[i]+","+
+                                        user_usn[i]+","+
+                                        user_phone[i]+","+
+                                        sslc[i]+","+
+                                        puc[i]+","+
+                                        sem1[i]+","+
+                                        sem2[i]+","+
+                                        sem3[i]+","+
+                                        sem4[i]+","+
+                                        sem5[i]+","+
+                                        sem6[i]+","+
+                                        sem7[i]+","+
+                                        cgpa[i]
+                        );
+                    }
+                    try
+                    {
+                        FileOutputStream out=ctx.openFileOutput("data.csv", Context.MODE_PRIVATE);
+                        out.write(data.toString().getBytes());
+                        out.close();
+
+                        //export
+
+
+                        File fileLocation=new File(ctx.getFilesDir(),"data.csv");
+                        Uri path= FileProvider.getUriForFile(ctx,"com.sanathls.sdmitplacement.fileprovider",fileLocation);
+                        Intent intent=new Intent(Intent.ACTION_SEND);
+                        intent.setType("text/csv");
+                        intent.putExtra(Intent.EXTRA_SUBJECT,"Users");
+                        intent.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
+                        intent.putExtra(Intent.EXTRA_STREAM,path);
+                        ctx.startActivity(intent);
+                    }
+                    catch(Exception e)
+                    {
+                        e.printStackTrace();
+                    }
+                }
+
+            } else {
+                Toast.makeText(ctx, "No Users", Toast.LENGTH_LONG).show();
             }
 
         } catch (JSONException e) {
